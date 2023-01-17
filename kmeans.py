cd7406b42106b7ff0a56c7d9ec799e31ae5df09e
jakubiee <sadgi.enjoyer@protonmail.com>
2023-01-17 06:37:49 +0100

    add kmeans

---
+++ kmeans.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+class Kmeans():
+    def kmeans(self, X, k, max_iterations=100, tolerance=1e-6):
+        centroids = X[np.random.choice(X.shape[0], k, replace=False), :]
+        print(centroids)
+        for i in range(max_iterations):
+            distances = np.array([self._distance(X, centroid) for centroid in centroids])
+            clusters = np.argmin(distances, axis=0)
+            new_centroids = np.array([X[clusters == j].mean(axis=0) for j in range(k)])
+            if np.linalg.norm(new_centroids - centroids) < tolerance:
+                break
+            centroids = new_centroids
+        return centroids, clusters
+
+    def _distance(self, X, centroid):
+        return np.linalg.norm(X - centroid, axis=1)
+
+
+
+
+X = np.random.rand(500, 3)
+a = Kmeans()
+centroids, clusters = a.kmeans(X, k=5)
+print(centroids, clusters)
